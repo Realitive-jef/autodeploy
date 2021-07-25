31275b7cc0732d1792a7a3807132a4b97b23dfae
Realitive-jef <82772868+Realitive-jef@users.noreply.github.com>
2021-07-25 14:26:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,77 +3,6 @@ const querystring = require('querystring');
 const discord = require('discord.js');
 const client = new discord.Client();
 
-// --
-
-http.createServer(function(req, res){
-  if (req.method == 'POST'){
-    var data = "";
-    req.on('data', function(chunk){
-      data += chunk;
-    });
-    req.on('end', function(){
-      if(!data){
-        res.end("No post data");
-        return;
-      }
-      var dataObject = querystring.parse(data);
-      console.log("post:" + dataObject.type);
-      if(dataObject.type == "wake"){
-        console.log("Woke up in post");
-        res.end();
-        return;
-      }
-      res.end();
-    });
-  }
-  else if (req.method == 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Discord Bot is active now\n');
-  }
-}).listen(3000);
-
-// ランダム
-
-client.on('ready', message =>{
-  console.log('Bot is Ready!');
-  client.user.setPresence({ activity: { name: 'モココ二シテヤルノ' } });
-});
-
-client
-  .on('ready' , () => console.log('ready'))
-  .on('message', message => {
-      if (message.author.bot) {
-          return;
-      }
-      if (message.mentions.users.find(user => user.id === client.user.id)) {
-           if (message.content.match(/ガチャ/))  {
-            const result = Math.floor(Math.random() * 20 + 1) * 10;
-            if (result < 200) {
-                message.reply(`う～ん。。。${result}連！`);
-            }
-          } else message.reply(`は～い`);
-      }
-});
-
-if(process.env.DISCORD_BOT_TOKEN == undefined){
- console.log('DISCORD_BOT_TOKEN　undefined');
- process.exit(0);
-}
-
-client.login( process.env.DISCORD_BOT_TOKEN );
-
-function sendReply(message, text){
-  message.reply(text)
-    .then(console.log("リプライ送信: " + text))
-    .catch(console.error);
-}
-
-function sendMsg(channelId, text, option={}){
-  client.channels.chache.get(channelId).send(text, option)
-    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
-    .catch(console.error);
-}
-
 // Youtube投稿通知
 
 const debugChannelId = "866832608386088990";
@@ -129,3 +58,46 @@ http.createServer(function(req, res){
     res.end('Discord bot is active now \n');
   }
 }).listen(3000);
+
+// ランダム
+
+client.on('ready', message =>{
+  console.log('Bot is Ready!');
+  client.user.setPresence({ activity: { name: 'モココ二シテヤルノ' } });
+});
+
+client
+  .on('ready' , () => console.log('ready'))
+  .on('message', message => {
+      if (message.author.bot) {
+          return;
+      }
+      if (message.mentions.users.find(user => user.id === client.user.id)) {
+           if (message.content.match(/ガチャ/))  {
+            const result = Math.floor(Math.random() * 20 + 1) * 10;
+            if (result < 200) {
+                message.reply(`う～ん。。。${result}連！`);
+            }
+          } else message.reply(`は～い`);
+      }
+});
+
+if(process.env.DISCORD_BOT_TOKEN == undefined){
+ console.log('DISCORD_BOT_TOKEN　undefined');
+ process.exit(0);
+}
+
+client.login( process.env.DISCORD_BOT_TOKEN );
+
+function sendReply(message, text){
+  message.reply(text)
+    .then(console.log("リプライ送信: " + text))
+    .catch(console.error);
+}
+
+function sendMsg(channelId, text, option={}){
+  client.channels.chache.get(channelId).send(text, option)
+    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
+    .catch(console.error);
+}
+
