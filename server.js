92f396012f752f4429a17073e0bd96feb91187c1
Realitive-jef <82772868+Realitive-jef@users.noreply.github.com>
2021-07-25 14:17:56 +0900

    add youtube_noctice

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const querystring = require('querystring');
 const discord = require('discord.js');
 const client = new discord.Client();
 
+// --
+
 http.createServer(function(req, res){
   if (req.method == 'POST'){
     var data = "";
@@ -29,6 +31,8 @@ http.createServer(function(req, res){
     res.end('Discord Bot is active now\n');
   }
 }).listen(3000);
+
+// ランダム
 
 client.on('ready', message =>{
   console.log('Bot is Ready!');
@@ -69,3 +73,59 @@ function sendMsg(channelId, text, option={}){
     .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
     .catch(console.error);
 }
+
+// Youtube投稿通知
+
+const debugChannelId = "866832608386088990";
+const mainChannelId = "866832608386088990";
+
+http.createServer(function(req, res){
+  if (req.method == 'POST') {
+    var data = "";
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+      if(!data){
+        console.log("No post data");
+        res.end();
+        return;
+      }
+      var dataObject = querystring.parse(data);
+      console.log("post:" + dataObject.type);
+      if(dataObject.type == "wake"){
+        console.log("Woke up in post");
+        res.end();
+        return;
+      }
+      if(dataObject.type == "newEruruVideo"){
+        let msgChannelId = debugChannelId;
+        if(dataObject.debug !== undefined && dataObject.debug == "false"){
+          msgChannelId = mainChannelId;
+        }
+        let msgMention = "<@270557414510690305>";
+        let videoId = dataObject.url.replace("https://youtu.be/", "");
+        let emb = {embed: {
+          author: {
+            name: "Eruru/えるるぅ",
+            url: "https://www.youtube.com/c/Eruru%E3%81%88%E3%82%8B%E3%82%8B%E3%81%85/featured",
+            icon_url: "https://yt3.ggpht.com/ytc/AKedOLRV6FUtQ_XIT9hNroje1k1mkf6kNOyqODSQw3P-HQ=s88-c-k-c0x00ffffff-no-rj"
+          },
+          title: dataObject.title,
+          url: dataObject.url,
+          description: dataObject.description,
+          color: 7506394,
+          timestamp: new Date(),
+          thumbnail: {
+            url: "http://img.youtube.com/vi/" + videoId + "/mqdefault.jpg"
+          }
+        }};
+        sendMsg(msgChannelId, msgMention + " の新着動画モコ！", emb);
+      }
+      res.end();
+    });
+  }else if (req.method == 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Discord bot is active now \n');
+  }
+}).listen(3000);
